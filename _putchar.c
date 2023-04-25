3bde856fce633556a8e30b5595ae0599a282b966
sibomana001 <sibomana.adolphe007@gmail.com>
2023-04-25 14:19:43 -0700

    printf team project

---
+++ _putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stddef.h>
+#include <string.h>
+/************* _putchar.c *************/
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
